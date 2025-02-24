66a6e5415c6e4a64a7efa20a22a5434a77874b3b
theMungai <mungaithesimon@gmail.com>
2025-02-25 00:52:29 +0300

    JavaScript Functions

---
+++ index.js
@@ -0,0 +1,26 @@
+function introduction(name){
+    return `Hi, my name is ${name}.`
+}
+introduction("Aki");
+
+
+
+function introductionWithLanguage(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+introductionWithLanguage("Gracie", "JavaScript");
+
+
+
+function introductionWithLanguageOptional(name, language){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+introductionWithLanguageOptional("Gracie", "JavaScript");
+
+
+
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+introductionWithLanguageOptional("Gracie");
